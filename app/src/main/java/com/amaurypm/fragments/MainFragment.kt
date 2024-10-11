31b7c124e26185bb4c299a981a9e1591034931b0
Amaury Perea Matsumura <amaury.matsumura@gmail.com>
2024-10-11 13:17:07 -0600

    Initial commit

---
+++ app/src/main/java/com/amaurypm/fragments/MainFragment.kt
@@ -0,0 +1,38 @@
+package com.amaurypm.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.amaurypm.fragments.databinding.FragmentMainBinding
+
+
+class MainFragment : Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMainBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnNext.setOnClickListener {
+
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+
+}
